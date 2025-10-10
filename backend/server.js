dd8b356b3946f3f98372f4bb12acc50540462cc8
harindinikeshala <harindinikeshala@gmail.com>
2025-10-10 19:05:35 +0530

    Setting nodemon

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@
 
   const app = express();
 
+  app.get("/api/notes", (req,res) => {
+    // Send the notes
+    res.status(200).send("Got 10 notes")
+  });
+
   app.listen(5001, () => {
     console.log('Server is running on port 5001');
   });
