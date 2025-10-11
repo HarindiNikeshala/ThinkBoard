b53c9156486aba516e2139998426e186321ece83
harindinikeshala <harindinikeshala@gmail.com>
2025-10-11 16:24:58 +0530

    Adding another controller

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,7 +2,7 @@ import Note from "../models/Note.js";
 
 export async function getAllNotes(req, res) {
     try {
-        const notes = await Note.find();
+        const notes = await Note.find().sort({ createdAt : -1}); //update order(newest first)
         res.status(200).json(notes);
     } catch (error) {
         console.error("Error in getAllNotes Controller", error);
@@ -13,6 +13,17 @@ export async function getAllNotes(req, res) {
 // export const getAllNotes = (req, res) => {
 //     res.status(200).send("You just fetch the notes.")
 // }
+
+export async function getNoteById(req, res) {
+    try {
+        const note = await Note.findById(req.params.id);
+        if (!note) return res.status(404).json({ message: "Note not found" });
+        res.status(200).json(note);
+    } catch (error) {
+        console.error("Error in getNoteById Controller", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
 
 export async function createNote(req, res) {
     try {
@@ -49,7 +60,7 @@ export async function deleteNote(req, res) {
 
         if (!deletedNote) return res.status(404).json({ message: "Note not found" });
         res.status(200).json({ message: "Note deleted Successfully" });
-        
+
     } catch (error) {
         console.error("Error in Delete Note Controller", error);
         res.status(500).json({ message: "Internal Server Error" });
