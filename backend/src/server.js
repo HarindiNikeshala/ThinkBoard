e611b408dba3528aa2d80d638721b7f4a96a7356
harindinikeshala <harindinikeshala@gmail.com>
2025-10-11 22:14:27 +0530

    Connecting database first

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,13 +8,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 // Middleware
-app.use(express.json());
+app.use(express.json()); //this middleware will parse JSON bodies: req.body
+
+//Simple custom middleware
+// app.use((req,res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// })
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log('Server is running on port: ',PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('Server is running on port: ',PORT);
+  });
 });
